31714ee337b5106a31e910a1306416936610b27f
Nataliia Glinska <glinska.nataliia21@gmail.com>
2020-01-18 15:52:40 -0500

    adding move forward function

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,10 +3,13 @@
 
 // ======================
 let rover = {
-  direction: "N"
+  direction: "N",
+  x : 0,
+  y : 0
  }
- 
+
 function turnLeft(rover){
+ // console.log("initial rover direction");
   switch (rover.direction){
     case "N":
       rover.direction = "W";
@@ -44,6 +47,41 @@ function turnRight(rover){
   console.log(`Rover is facing now to ${rover.direction}`);
 }
 function moveForward(rover){
-  console.log("moveForward was called")
-}
+
+  switch(rover.direction){
+    case "N":
+      //rover.y = rover.y -1;
+      rover.y--;
+      break;
+    case "W":
+      rover.x--;
+      break;
+    case "S":
+      rover.y++;
+      break;
+    case "E":
+      rover.x++;
+      break;
+    case "STOP":
+      console.log("You are at the edge, cannot go outside boundary");
+      break;
+  }
+  console.log("moveForward was called");
+
+  if(rover.x < 0 || rover.y < 0 || rover.x > 9 || rover.y > 9 ){
+    if (rover.x < 0) {
+      rover.x++;
+    } else if (rover.y < 0) {
+      rover.y++;
+    } else if (rover.y > 9) {
+      rover.y--;
+    } else if (rover.x > 9) {
+      rover.x--;
+    }     
+  } else {
+      console.log("moveForward was called");
+      console.log("Rover position is: (x=" + rover.x + ", y=" + rover.y + ").");
+      }
+  }
+
 
